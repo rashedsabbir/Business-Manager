a0a36ffa806f534af1009eff2c05d3a9391270e8
rashedsabbir <rashedulhasan1996@gmail.com>
2022-11-05 13:16:30 +0600

    home added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './Components/Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Coming soon</h1>
+      <Home/>
     </div>
   );
 }
