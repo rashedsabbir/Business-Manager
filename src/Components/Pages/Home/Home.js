88cd4f017adba865119a4a579c8e5e3b87c0aeb6
rashedsabbir <rashedulhasan1996@gmail.com>
2022-11-09 02:37:59 +0600

    visitor and admin image added

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import visitor from '../../../Images/Vector/visitor_art.jpg'
+import admin from '../../../Images/Vector/admin_art.jpg'
 
 const Home = () => {
     return (
@@ -16,8 +18,8 @@ const Home = () => {
                   class="px-8 pb-8 transition-opacity group-hover:absolute group-hover:opacity-0"
                 >
                   <img
-                alt="Developer"
-                src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+                alt="visitor"
+                src={visitor}
                 class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
               />
                   <svg
@@ -64,8 +66,8 @@ const Home = () => {
       class="px-8 pb-8 transition-opacity group-hover:absolute group-hover:opacity-0"
     >
       <img
-    alt="Developer"
-    src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+    alt="admin"
+    src={admin}
     class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
   />
       <svg
