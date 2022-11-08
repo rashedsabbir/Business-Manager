ccc3374293d07ab2835e75b41bb79adb3c1b416e
rashedsabbir <rashedulhasan1996@gmail.com>
2022-11-08 14:59:05 +0600

    second card added

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -2,22 +2,72 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='px-72 py-24'>
+        <div className='lg:mx-36 mx-6 py-24'>
+            <div className='grid lg:grid-cols-2 grid-rows-2 gap-12'>
+            <a href="" class="group relative block h-96">
+              
+              <span class="absolute inset-0 border-2 border-dashed border-black"></span>
+            
+              <div
+                class="relative flex  h-full transform items-end border-2 border-black bg-white transition-transform  group-hover:-translate-x-2 group-hover:-translate-y-2"
+              >
+                
+                <div
+                  class="px-8 pb-8 transition-opacity group-hover:absolute group-hover:opacity-0"
+                >
+                  <img
+                alt="Developer"
+                src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+                class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+              />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-12 w-12"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+            
+                  <h2 class="mt-4 text-2xl font-medium">Go around the world</h2>
+                </div>
+            
+                <div
+                  class="absolute p-8 opacity-0  transition-opacity group-hover:relative group-hover:opacity-100"
+                >
+                  <h3 class="mt-4 text-2xl font-medium">Go around the world</h3>
+            
+                  <p class="mt-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+                    praesentium voluptatem omnis atque culpa repellendus.
+                  </p>
+            
+                  <p class="mt-8 font-bold">Read more</p>
+                </div>
+              </div>
+            </a>
             <a href="" class="group relative block h-96">
               
   <span class="absolute inset-0 border-2 border-dashed border-black"></span>
 
   <div
-    class="relative flex  h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2"
+    class="relative flex  h-full transform items-end border-2 border-black bg-white transition-transform  group-hover:-translate-x-2 group-hover:-translate-y-2"
   >
-    <img
+    
+    <div
+      class="px-8 pb-8 transition-opacity group-hover:absolute group-hover:opacity-0"
+    >
+      <img
     alt="Developer"
     src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
     class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
   />
-    <div
-      class="px-8 pb-8 transition-opacity group-hover:absolute group-hover:opacity-0"
-    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-12 w-12"
@@ -37,7 +87,7 @@ const Home = () => {
     </div>
 
     <div
-      class="absolute p-8 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100"
+      class="absolute p-8 opacity-0  transition-opacity group-hover:relative group-hover:opacity-100"
     >
       <h3 class="mt-4 text-2xl font-medium">Go around the world</h3>
 
@@ -50,6 +100,7 @@ const Home = () => {
     </div>
   </div>
 </a>
+            </div>
 
         </div>
     );
